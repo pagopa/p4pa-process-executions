8b4887b58ab1ed0d770a6c93904172c28fbf3ac0
MScarsella <marco.scarsella@emeal.nttdata.com>
2024-12-20 18:17:56 +0100

    feat: P4ADEV-1812-create-ingestion-flow-file (#2)
    Co-authored-by: mscarsel <marco.scarsella@nttdata.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,6 +36,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
   implementation("io.micrometer:micrometer-tracing-bridge-otel:$micrometerVersion")
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:$springDocOpenApiVersion")
 	implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
@@ -109,6 +110,6 @@ openApiGenerate {
     "useTags" to "true",
     "generateConstructorWithAllArgs" to "false",
     "generatedConstructorWithRequiredArgs" to "true",
-    "additionalModelTypeAnnotations" to "@lombok.Data @lombok.Builder @lombok.AllArgsConstructor"
+    "additionalModelTypeAnnotations" to "@lombok.Builder @lombok.AllArgsConstructor"
   ))
 }
