5de77a99cb16600dbb8d2719af21c1a25cc01444
antonio.torre <antonio.torre@pagopa.it>
2025-01-29 09:54:54 +0100

    P4ADEV-2048 ExportFile model

--- src/main/java/it/gov/pagopa/pu/processecexutions/model/exportfile/ClassificationsExportFileFilter.java
+++ src/main/java/it/gov/pagopa/pu/processecexutions/model/exportfile/ClassificationsExportFileFilter.java
@@ -1,5 +1,6 @@
 package it.gov.pagopa.pu.processecexutions.model.exportfile;
 
+import it.gov.pagopa.pu.processecexutions.dto.LocalDateIntervalFilter;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,5 +9,22 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ClassificationsExportFileFilter implements ExportFileFilter {
+  private String iuf;
+  private String iuv;
+  private String iur;
+
+  private String debtPositionTypeCode;
+
   private String label;
+
+  private LocalDateIntervalFilter lastClassificationDate;
+  private LocalDateIntervalFilter regulationDate;
+  private LocalDateIntervalFilter billDate;
+  private LocalDateIntervalFilter regionValueDate;
+
+  private String debtorUniqueIdentifierCode;
+  private String payerUniqueIdentifierCode;
+  private String pspLastName;
+  private String accountRegistryCode;
+  private String billAmountCents;
 }
