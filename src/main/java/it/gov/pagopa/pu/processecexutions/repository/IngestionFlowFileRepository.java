ffdf2beb3746e347776341d124847086d1b60beb
mscarsel <marco.scarsella@nttdata.com>
2025-01-31 09:58:00 +0100

    [P4ADEV-1941] updated query IngestionFlowFileRepository.findByOrganizationIDFlowTypeCreateDate

--- src/main/java/it/gov/pagopa/pu/processecexutions/repository/IngestionFlowFileRepository.java
+++ src/main/java/it/gov/pagopa/pu/processecexutions/repository/IngestionFlowFileRepository.java
@@ -1,5 +1,6 @@
 package it.gov.pagopa.pu.processecexutions.repository;
 
+import io.swagger.v3.oas.annotations.Parameter;
 import it.gov.pagopa.pu.processecexutions.enums.IngestionFlowFileStatus;
 import it.gov.pagopa.pu.processecexutions.enums.IngestionFlowFileType;
 import it.gov.pagopa.pu.processecexutions.model.IngestionFlowFile;
@@ -10,6 +11,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.transaction.annotation.Transactional;
@@ -31,19 +33,9 @@ public interface IngestionFlowFileRepository extends JpaRepository<IngestionFlow
     + "FROM IngestionFlowFile iff "
     + "WHERE iff.organizationId = :organizationId "
     + "AND iff.flowFileType = :flowFileType "
-    + "AND (:creationDate IS NULL OR iff.creationDate > :creationDate) "
-    + "AND (:filename IS NULL OR iff.fileName = :fileName) ")
-  Page<IngestionFlowFile> findByOrganizationIDFlowTypeCreateDate(Long organizationId, IngestionFlowFileType flowFileType, LocalDateTime creationDate, String fileName, Pageable pageable);
-
-  @Query("SELECT iff "
-    + "FROM IngestionFlowFile iff "
-    + "WHERE (:organizationId IS NULL OR iff.organizationId = :organizationId) "
-    + "AND (:flowFileType IS NULL OR iff.flowFileType = :flowFileType) "
+    + "AND (cast(:creationDateFrom as date) IS NULL OR iff.creationDate >= :creationDateFrom) "
+    + "AND (cast(:creationDateTo as date) IS NULL OR iff.creationDate <= :creationDateTo) "
     + "AND (:fileName IS NULL OR iff.fileName ILIKE CONCAT('%', cast(:fileName as text), '%')) "
-    + "AND ((cast(:fromCreationDate as date) IS NULL AND cast(:toCreationDate as date) IS NULL) "
-      + "OR (cast(:fromCreationDate as date) IS NULL AND cast(:toCreationDate as date) IS NOT NULL AND iff.creationDate <= :toCreationDate) "
-      + "OR (cast(:toCreationDate as date) IS NULL AND cast(:fromCreationDate as date) IS NOT NULL AND iff.creationDate >= :fromCreationDate) "
-      + "OR (iff.creationDate BETWEEN :fromCreationDate AND :toCreationDate)) "
     + "AND (:status IS NULL OR iff.status = :status) ")
-  Page<IngestionFlowFile> findIngestionFlowFiles(Long organizationId,IngestionFlowFileType flowFileType,String fileName,LocalDateTime fromCreationDate,LocalDateTime toCreationDate,IngestionFlowFileStatus status, Pageable pageable);
+  Page<IngestionFlowFile> findByOrganizationIDFlowTypeCreateDate(@Parameter(required = true) @Param("organizationId") Long organizationId,@Parameter(required = true) @Param("flowFileType") IngestionFlowFileType flowFileType, LocalDateTime creationDateFrom, LocalDateTime creationDateTo, IngestionFlowFileStatus status, String fileName, Pageable pageable);
 }
