4fbe80d4c6fbefbd26666c4b32fa95afeb2cc5b0
MScarsella <marco.scarsella@emeal.nttdata.com>
2025-01-31 16:31:17 +0100

    feat: P4ADEV-1941-added-query-findIngestionFlowFiles (#14)
    Co-authored-by: mscarsel <marco.scarsella@nttdata.com>

--- src/main/java/it/gov/pagopa/pu/processecexutions/repository/IngestionFlowFileRepository.java
+++ src/main/java/it/gov/pagopa/pu/processecexutions/repository/IngestionFlowFileRepository.java
@@ -1,19 +1,20 @@
 package it.gov.pagopa.pu.processecexutions.repository;
 
+import io.swagger.v3.oas.annotations.Parameter;
 import it.gov.pagopa.pu.processecexutions.enums.IngestionFlowFileStatus;
 import it.gov.pagopa.pu.processecexutions.enums.IngestionFlowFileType;
 import it.gov.pagopa.pu.processecexutions.model.IngestionFlowFile;
 import jakarta.annotation.Nonnull;
+import java.time.LocalDateTime;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.time.LocalDateTime;
 
 @RepositoryRestResource(path = "ingestion-flow-files")
 public interface IngestionFlowFileRepository extends JpaRepository<IngestionFlowFile, Long> {
@@ -32,7 +33,9 @@ public interface IngestionFlowFileRepository extends JpaRepository<IngestionFlow
     + "FROM IngestionFlowFile iff "
     + "WHERE iff.organizationId = :organizationId "
     + "AND iff.flowFileType = :flowFileType "
-    + "AND (:creationDate IS NULL OR iff.creationDate > :creationDate) "
-    + "AND (:filename IS NULL OR iff.fileName = :fileName) ")
-  Page<IngestionFlowFile> findByOrganizationIDFlowTypeCreateDate(Long organizationId, IngestionFlowFileType flowFileType, LocalDateTime creationDate, String fileName, Pageable pageable);
+    + "AND (cast(:creationDateFrom as date) IS NULL OR iff.creationDate >= :creationDateFrom) "
+    + "AND (cast(:creationDateTo as date) IS NULL OR iff.creationDate <= :creationDateTo) "
+    + "AND (:fileName IS NULL OR iff.fileName ILIKE CONCAT('%', cast(:fileName as text), '%')) "
+    + "AND (:status IS NULL OR iff.status = :status) ")
+  Page<IngestionFlowFile> findByOrganizationIDFlowTypeCreateDate(@Parameter(required = true) @Param("organizationId") Long organizationId,@Parameter(required = true) @Param("flowFileType") IngestionFlowFileType flowFileType, LocalDateTime creationDateFrom, LocalDateTime creationDateTo, IngestionFlowFileStatus status, String fileName, Pageable pageable);
 }
