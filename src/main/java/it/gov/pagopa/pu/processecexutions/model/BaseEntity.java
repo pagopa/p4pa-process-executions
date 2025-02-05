820defdb373c1b5dc527f17bc0cace68dd5dabf7
antonioT90 <34568575+antonioT90@users.noreply.github.com>
2025-02-05 20:05:35 +0100

    fix: ErrorHandling and LocalDateTime json serialization (#20)

--- src/main/java/it/gov/pagopa/pu/processecexutions/model/BaseEntity.java
+++ src/main/java/it/gov/pagopa/pu/processecexutions/model/BaseEntity.java
@@ -1,7 +1,5 @@
 package it.gov.pagopa.pu.processecexutions.model;
 
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import it.gov.pagopa.pu.processecexutions.config.json.LocalDateTimeToOffsetDateTimeSerializer;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
@@ -26,10 +24,8 @@ import java.time.LocalDateTime;
 public abstract class BaseEntity implements Serializable {
   @Column(updatable = false)
   @CreatedDate
-  @JsonSerialize(using = LocalDateTimeToOffsetDateTimeSerializer.class)
   private LocalDateTime creationDate;
   @LastModifiedDate
-  @JsonSerialize(using = LocalDateTimeToOffsetDateTimeSerializer.class)
   private LocalDateTime updateDate;
   @LastModifiedBy
   private String updateOperatorExternalId;
