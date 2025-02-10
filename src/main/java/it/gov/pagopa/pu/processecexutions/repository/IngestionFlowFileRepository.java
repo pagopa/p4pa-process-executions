7e9d37cc041a21d10226900a678f6df1af172685
MScarsella <marco.scarsella@emeal.nttdata.com>
2025-02-10 14:19:52 +0100

    feat: P4ADEV-2123-updated-findByOrganizationIDFlowTypeCreateDate (#21)
    Co-authored-by: mscarsel <marco.scarsella@nttdata.com>

--- src/main/java/it/gov/pagopa/pu/processecexutions/repository/IngestionFlowFileRepository.java
+++ src/main/java/it/gov/pagopa/pu/processecexutions/repository/IngestionFlowFileRepository.java
@@ -1,11 +1,14 @@
 package it.gov.pagopa.pu.processecexutions.repository;
 
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Schema;
 import it.gov.pagopa.pu.processecexutions.enums.IngestionFlowFileStatus;
 import it.gov.pagopa.pu.processecexutions.enums.IngestionFlowFileType;
 import it.gov.pagopa.pu.processecexutions.model.IngestionFlowFile;
 import jakarta.annotation.Nonnull;
 import java.time.LocalDateTime;
+import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -38,11 +41,18 @@ public interface IngestionFlowFileRepository extends JpaRepository<IngestionFlow
   @Query("SELECT iff "
     + "FROM IngestionFlowFile iff "
     + "WHERE iff.organizationId = :organizationId "
-    + "AND iff.flowFileType = :flowFileType "
+    + "AND iff.flowFileType IN :flowFileTypes "
     + "AND (cast(:creationDateFrom as date) IS NULL OR iff.creationDate >= :creationDateFrom) "
     + "AND (cast(:creationDateTo as date) IS NULL OR iff.creationDate <= :creationDateTo) "
     + "AND (:fileName IS NULL OR iff.fileName ILIKE CONCAT('%', cast(:fileName as text), '%')) "
     + "AND (:status IS NULL OR iff.status = :status) "
     + "AND (:operatorExternalId IS NULL OR iff.operatorExternalId = :operatorExternalId) ")
-  Page<IngestionFlowFile> findByOrganizationIDFlowTypeCreateDate(@Parameter(required = true) @Param("organizationId") Long organizationId,@Parameter(required = true) @Param("flowFileType") IngestionFlowFileType flowFileType, LocalDateTime creationDateFrom, LocalDateTime creationDateTo, IngestionFlowFileStatus status, String fileName, String operatorExternalId, Pageable pageable);
+  Page<IngestionFlowFile> findByOrganizationIDFlowTypeCreateDate(@Parameter(required = true) @Param("organizationId") Long organizationId,
+    @Parameter(required = true, array = @ArraySchema(schema = @Schema(type = "string"))) @Param("flowFileTypes") List<IngestionFlowFileType> flowFileTypes,
+    @Parameter(schema = @Schema(type = "LocalDateTime")) @Param("creationDateFrom") LocalDateTime creationDateFrom,
+    @Parameter(schema = @Schema(type = "LocalDateTime")) @Param("creationDateTo") LocalDateTime creationDateTo,
+    IngestionFlowFileStatus status,
+    String fileName,
+    String operatorExternalId,
+    Pageable pageable);
 }
