25734fe2644b96378f50630bf93cbf1795bc5ad2
antonio.torre <antonio.torre@pagopa.it>
2025-03-21 18:28:50 +0100

    fix: Exposing enum as ref

--- src/main/java/it/gov/pagopa/pu/processecexutions/enums/ExportFileStatus.java
+++ src/main/java/it/gov/pagopa/pu/processecexutions/enums/ExportFileStatus.java
@@ -1,5 +1,8 @@
 package it.gov.pagopa.pu.processecexutions.enums;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(enumAsRef = true)
 public enum ExportFileStatus {
   REQUESTED,
   PROCESSING,
