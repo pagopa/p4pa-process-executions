2c8033bc5e0e78f1bc988bdd062c460f11c5ee35
RiccardoGiuliani <riccardo.giuliani@nttdata.com>
2025-10-06 12:08:55 +0200

    P4ADEV-3909 fix available version

---
+++ src/main/java/it/gov/pagopa/pu/processecexutions/enums/ingestion/version/PaymentsReportingPagoPaIngestionFlowFileVersion.java
@@ -0,0 +1,26 @@
+package it.gov.pagopa.pu.processecexutions.enums.ingestion.version;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(enumAsRef = true)
+public enum PaymentsReportingPagoPaIngestionFlowFileVersion implements VersionEnum {
+  V1_0("1.0");
+
+  private final String value;
+
+  PaymentsReportingPagoPaIngestionFlowFileVersion(String value) {
+    this.value = value;
+  }
+
+  @JsonCreator
+  public static PaymentsReportingPagoPaIngestionFlowFileVersion forValue(String value) {
+    return VersionEnumUtils.fromValue(PaymentsReportingPagoPaIngestionFlowFileVersion.class, value);
+  }
+
+  @JsonValue
+  public String getValue() {
+    return value;
+  }
+}
