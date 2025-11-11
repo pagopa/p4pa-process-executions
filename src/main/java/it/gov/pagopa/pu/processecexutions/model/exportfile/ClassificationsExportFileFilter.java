6d44937720e9bf1adc5cd70d4e511f8829e94e51
michele.mazzini <michele.mazzini1996@gmail.com>
2025-11-11 08:47:46 +0100

    fix: fix export filter

--- src/main/java/it/gov/pagopa/pu/processecexutions/model/exportfile/ClassificationsExportFileFilter.java
+++ src/main/java/it/gov/pagopa/pu/processecexutions/model/exportfile/ClassificationsExportFileFilter.java
@@ -16,7 +16,7 @@ import java.util.Set;
 @AllArgsConstructor
 public class ClassificationsExportFileFilter implements ExportFileFilter {
 
-  private List<String> iuf;
+  private List<String> iufs;
   private String iud;
   private List<String> iuv;
   private List<String> iur;
