0553c4954c48f97f25ddd88f52c14fb2ac633ccb
antonio.torre <antonio.torre@pagopa.it>
2025-12-18 12:50:53 +0100

    fix: Fix CVE, upgrade SpringBoot 4

--- src/main/java/it/gov/pagopa/pu/processecexutions/config/json/JsonConfig.java
+++ src/main/java/it/gov/pagopa/pu/processecexutions/config/json/JsonConfig.java
@@ -13,6 +13,7 @@ import it.gov.pagopa.pu.processecexutions.config.json.jackson3.LocalDateTimeToOf
 import it.gov.pagopa.pu.processecexutions.config.json.jackson3.OffsetDateTimeToLocalDateTimeJackson3Deserializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import tools.jackson.databind.JacksonModule;
 import tools.jackson.databind.json.JsonMapper;
 
@@ -21,6 +22,7 @@ import java.time.OffsetDateTime;
 import java.util.TimeZone;
 
 @Configuration
+@EnableSpringDataWebSupport
 public class JsonConfig {
 
   @Bean
