391dacc9b77738997b7122185b20f26971c63522
antonio.torre <antonio.torre@pagopa.it>
2026-03-05 19:41:13 +0100

    fix: Fix CVE

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,15 +6,15 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
   java
-  id("org.springframework.boot") version "4.0.0"
+  id("org.springframework.boot") version "4.0.3"
   id("io.spring.dependency-management") version "1.1.7"
   jacoco
-  id("org.sonarqube") version "7.2.1.6560"
+  id("org.sonarqube") version "7.2.3.7755"
   id("com.github.ben-manes.versions") version "0.53.0"
-  id("org.openapi.generator") version "7.17.0"
+  id("org.openapi.generator") version "7.20.0"
   id("org.ajoberstar.grgit") version "5.3.2"
-  id("com.gorylenko.gradle-git-properties") version "2.5.4"
-  id("com.github.jk1.dependency-license-report") version "3.0.1"
+  id("com.gorylenko.gradle-git-properties") version "2.5.7"
+  id("com.github.jk1.dependency-license-report") version "3.1.1"
 }
 
 group = "it.gov.pagopa.payhub"
@@ -50,14 +50,18 @@ repositories {
   mavenCentral()
 }
 
-val springDocOpenApiVersion = "3.0.0"
+val springDocOpenApiVersion = "3.0.2"
 val janinoVersion = "3.1.12"
-val openApiToolsVersion = "0.2.8"
-val micrometerVersion = "1.6.1"
+val openApiToolsVersion = "0.2.9"
+val micrometerVersion = "1.6.3"
 val bouncycastleVersion = "1.83"
-val postgresJdbcVersion = "42.7.8"
-val httpClientVersion = "5.5.1"
+val postgresJdbcVersion = "42.7.10"
+val httpClientVersion = "5.6"
 val commonsLang3Version = "3.20.0"
+
+// fix cve
+val jackson2CoreVersion = "2.21.1"
+val jackson3CoreVersion = "3.1.0"
 
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webmvc")
@@ -81,6 +85,10 @@ dependencies {
   implementation("org.bouncycastle:bcprov-jdk18on:$bouncycastleVersion")
   implementation("org.postgresql:postgresql:$postgresJdbcVersion")
   implementation("org.apache.httpcomponents.client5:httpclient5:$httpClientVersion")
+
+  // CVE fix
+  implementation("tools.jackson.core:jackson-core:$jackson3CoreVersion")
+  implementation("com.fasterxml.jackson.core:jackson-core:$jackson2CoreVersion")
 
   compileOnly("org.projectlombok:lombok")
   annotationProcessor("org.projectlombok:lombok")
